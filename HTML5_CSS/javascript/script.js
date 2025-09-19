3dbeb947a7e32b5a22bd18285b43c533dfb1f836
Eli-Noles <enoles1@my.chattanoogastate.edu>
2025-09-18 20:24:31 -0400

    yataa-yataa

--- HTML5_CSS/javascript/script.js
+++ HTML5_CSS/javascript/script.js
@@ -1,11 +1,13 @@
-const dropdownBtn = document.getElementById("menuu");
+const dropdownBtns = document.querySelectorAll(".menuu");
 const dropdownMenu = document.getElementById("nav");
 
 const toggleDropdown = function () {
   dropdownMenu.classList.toggle("show");
 };
 
-dropdownBtn.addEventListener("click", function (e) {
-  e.stopPropagation();
-  toggleDropdown();
+dropdownBtns.forEach(btn => {
+  btn.addEventListener("click", function (e) {
+    e.stopPropagation();
+    toggleDropdown();
+  });
 });
